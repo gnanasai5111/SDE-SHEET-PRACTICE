23b6aefb683da9018604129f991706930058b125
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2024-05-10 09:20:19 +0530

    Update palindromeLinkedList.java

--- LinkedList/palindromeLinkedList.java
+++ LinkedList/palindromeLinkedList.java
@@ -34,3 +34,45 @@ class Solution {
 }
 Time complexity - o(N)
 Space complexity - o(N)
+
+
+Optimised approach :
+
+Using Fast and slow pointer find the mid element,and get the reverse of the half from that mid.
+Now keep two pointers one at the start of head and other at reverse of the half from mid.
+Now compare elements and check if they are equal or not
+
+class Solution {
+    public ListNode reverse(ListNode node){
+        ListNode curr=node;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode p1=head;
+        ListNode p2=reverse(slow);
+        while(p1!=null && p2!=null){
+            if(p1.val!=p2.val){
+                return false;
+            }
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return true;
+    }
+}
+
+Time complexity - o(N)
+Space complexity - o(1)
