ad705322bf0de2f3df703dce2571ef06717292b2
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2024-05-16 17:40:30 +0530

    Update TrappingRainWater.java

--- Arrays/TrappingRainWater.java
+++ Arrays/TrappingRainWater.java
@@ -82,3 +82,55 @@ class Solution {
 Time complexity - o(N)
 Space compelxity - o(N)
 
+
+Optimisec approach - Two Pointer
+
+Take 2 pointers l(left pointer) and r(right pointer) pointing to 0th and (n-1)th index respectively. 
+Take two variables leftMax and rightMax and initialize them to 0. 
+If height[l] is less than or equal to height[r] then if leftMax is less than height[l] update leftMax to height[l]
+else add leftMax-height[l] to your final answer and move the l pointer to the right i.e l++. 
+If height[r] is less than height[l], then now we are dealing with the right block.
+If height[r] is greater than rightMax, then update rightMax to height[r]
+else add rightMax-height[r] to the final answer. 
+Now move r to the left. Repeat these steps till l and r crosses each other.
+
+We need a minimum of leftMax and rightMax.
+So if we take the case when height[l]<=height[r] we increase l++,
+so we can surely say that there is a block with a height more than height[l] to the right of l. 
+And for the same reason when height[r]<=height[l] we can surely say that there is a block to the left of r 
+which is at least of height[r]. So by traversing these cases and using two pointers approach the time complexity
+can be decreased without using extra space.
+
+Code:
+
+class Solution {
+    public int trap(int[] height) {
+        int count=0;
+        int l=0,r=height.length-1;
+        int leftMax=0,rightMax=0;
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l]>leftMax){
+                    leftMax=height[l];
+                }
+                else{
+                    count+=leftMax-height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r]>rightMax){
+                    rightMax=height[r];
+                }
+                else{
+                    count+=rightMax-height[r];
+                }
+                r--;
+            }
+        }
+        return count;
+    }
+}
+
+Time complexity - o(N)
+Space compelxity - o(1)
