e09577d48daac692e92eb1d1d1d1608f460fc3fa
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2024-06-10 18:04:33 +0530

    Update Nqueens.java

--- Recursion/Nqueens.java
+++ Recursion/Nqueens.java
@@ -84,3 +84,59 @@ class Solution {
 
 Time complexity - O(N! * N). (exponential)
 Space complexity - o(N^2)
+
+
+Optimised approach :
+
+This is the optimization of the issafe function. In the previous issafe function, we need o(N) for a row, o(N) for the column, 
+and o(N) for the diagonal.
+Here, we will use hashing to maintain a list to check whether that position can be the right one or not.
+
+class Solution {
+    public List<String> constructBoard(char board[][]) {
+        List<String> each = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            each.add(s);
+        }
+        return each;
+    }
+
+    public void dfs(int col, char board[][], List<List<String>> res,int leftCol[],int lowerDiagonal[],int upperDiagonal[]) {
+        if (col == board.length) {
+            res.add(constructBoard(board));
+            return;
+        }
+        for (int row = 0; row < board.length; row++) {
+            if (leftCol[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[board.length - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftCol[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+                dfs(col + 1, board, res, leftCol, lowerDiagonal, upperDiagonal);
+                board[row][col] = '.';
+                leftCol[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        int leftCol[] = new int[n];
+        int lowerDiagonal[] = new int[2 * n - 1];
+        int upperDiagonal[] = new int[2 * n - 1];
+        List<List<String>> res = new ArrayList<>();
+        dfs(0, board, res, leftCol, lowerDiagonal, upperDiagonal);
+        return res;
+    }
+}
+
+Time complexity - O(N! * N). (exponential)
+Space complexity - o(N)
