0fc4a76106ff45ba0f7cefc32fb06251e98c7a33
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2024-06-11 22:10:24 +0530

    Create sudokuSolver.java

---
+++ Recursion/sudokuSolver.java
@@ -0,0 +1,77 @@
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+ 
+
+Example 1:
+
+
+Input: board = [["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],
+  ["6","7","2","1","9","5","3","4","8"],
+  ["1","9","8","3","4","2","5","6","7"],
+  ["8","5","9","7","6","1","4","2","3"],
+  ["4","2","6","8","5","3","7","9","1"],
+  ["7","1","3","9","2","4","8","5","6"],
+  ["9","6","1","5","3","7","2","8","4"],
+  ["2","8","7","4","1","9","6","3","5"],
+  ["3","4","5","2","8","6","1","7","9"]]
+
+  Approach :
+
+  Since we have to fill the empty cells with available possible numbers and we can also have multiple solutions,
+  the main intuition is to try every possible way of filling the empty cells. And the more correct way to try all possible
+  solutions is to use recursion. In each call to the recursive function, we just try all the possible numbers for a
+  particular cell and transfer the updated board to the next recursive call.
+
+  class Solution {
+    public boolean isValid(int row,int col,char c,char board[][]){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c || board[row][i]==c || board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean solve(char board[][]){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(isValid(i,j,c,board)){
+                            board[i][j]=c;
+                            if(solve(board)){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+}
+
+Time complexity -o(9^(n ^ 2))
+Space complexity - o(1)
